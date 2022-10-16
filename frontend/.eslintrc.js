02d302ac69589d5ae796091c757b1eca7e104d9a
julian-saez <juliansaez.projects@gmail.com>
2022-10-16 05:40:38 -0300

    feat: adding frontend and backend folders

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+module.exports = {
+    extends: [
+        require.resolve( 'solidjs-eslint-config' ),
+    ],
+    rules: {
+        'jsx-quotes': [ 'error', 'prefer-double' ],
+    },
+} ;
